24a9bded5810403cd98c08dd87fe595e9881eb02
dinesh <veerapareddydinesh@gmail.com>
2024-12-15 08:48:04 +0530

    1

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import './App.css';
+const Lazyhome = React.lazy( () => import('./pages/Home'))
+import Notfound from './utils/not-found';
+import Profile from './pages/auth/Profile/Profile';
+
+function App() {
+  return (
+    
+      <Routes>
+       
+        <Route path="/" element={
+           <React.Suspense fallback={<div>Loading...</div>}>
+           <Lazyhome />
+           </React.Suspense>
+        } />
+       
+        <Route path="/newroutre"  >
+        <Route index />
+        </Route>
+        <Route path="users/:userid"  />
+        <Route path='profile' element={<Profile />} />
+        
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+  
+  );
+}
+
+export default App;
